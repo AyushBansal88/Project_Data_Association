27423eb08d81e9419100852fc6aeab71611a00eb
Ayush Bansal <ayushbansal0588@gmail.com>
2026-01-13 16:04:26 +0530

    atalas connected

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require("cors");
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
@@ -7,6 +8,7 @@ const userRoute = require('./routes/userRoute');
 const postRoute = require('./routes/postRoute');
 
 app.set('view engine', 'ejs');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
